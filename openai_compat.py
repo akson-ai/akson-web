b70e548fb929590ad7fce74d697eaea78c670653
Cenk Altı <cenkalti@gmail.com>
2024-09-21 18:18:21 -0400

    add fake streaming response

--- openai_compat.py
+++ openai_compat.py
@@ -1,8 +1,11 @@
+import time
+import json
 import random
 from typing import Literal
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
+from sse_starlette import EventSourceResponse
 from openai.types.chat import (
     ChatCompletionMessageParam,
     ChatCompletionSystemMessageParam,
@@ -21,6 +24,7 @@ class Message(BaseModel):
 class ChatCompletionRequest(BaseModel):
     model: str
     messages: list[Message]
+    stream: bool | None = False
     temperature: float = Field(default=1.0, ge=0.0, le=2.0)
 
 
@@ -45,6 +49,22 @@ class ChatCompletionResponse(BaseModel):
     usage: Usage
 
 
+def chat_streaming_chunk(response: ChatCompletionResponse, content: str, *, finish_reason: str | None = None):
+    return {
+        "id": response.id,
+        "object": "chat.completions.chunk",
+        "created": response.created,
+        "model": response.model,
+        "choices": [
+            {
+                "index": 0,
+                "finish_reason": finish_reason,
+                "delta": {"role": "assistant", "content": content},
+            }
+        ],
+    }
+
+
 def setup_routes(app: FastAPI, agents: dict[str, Agent]):
     async def chat_completions(request: ChatCompletionRequest):
         try:
@@ -58,10 +78,10 @@ def setup_routes(app: FastAPI, agents: dict[str, Agent]):
         completion_tokens = len(content.split())
         prompt_tokens = sum(len(msg.content.split()) for msg in request.messages)
 
-        return ChatCompletionResponse(
+        response = ChatCompletionResponse(
             id=f"chatcmpl-{random.randint(1000000, 9999999)}",
             object="chat.completion",
-            created=int(random.random() * 1000000000),
+            created=int(time.time()),
             model=request.model,
             choices=[Choice(index=0, message=Message(role="assistant", content=content), finish_reason="stop")],
             usage=Usage(
@@ -70,6 +90,21 @@ def setup_routes(app: FastAPI, agents: dict[str, Agent]):
                 total_tokens=prompt_tokens + completion_tokens,
             ),
         )
+
+        if not request.stream:
+            return response
+
+        async def generator():
+            yield {"data": json.dumps(chat_streaming_chunk(response, ""))}
+
+            # Fake streaming of whole content
+            for word in content.split():
+                yield {"data": json.dumps(chat_streaming_chunk(response, word + " "))}
+
+            yield {"data": json.dumps(chat_streaming_chunk(response, "", finish_reason="stop"))}
+            yield "[DONE]"
+
+        return EventSourceResponse(generator())
 
     app.add_api_route("/v1/chat/completions", chat_completions, methods=["POST"], response_model=ChatCompletionResponse)
 
