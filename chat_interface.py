6ff8c677133c811748645439da786dc02ca41686
Cenk Altı <cenkalti@gmail.com>
2024-10-13 00:17:03 -0400

    upgrade gradio

--- chat_interface.py
+++ chat_interface.py
@@ -4,9 +4,7 @@ from agent import Agent
 
 
 def create(agent: Agent) -> gr.ChatInterface:
-    def handle_message(message: str, history: list[tuple[str, str]]) -> str:
+    def handle_message(message: str, history: list[dict[str, str]]) -> str:
         return agent.message(message, session_id="gradio")
 
-    return gr.ChatInterface(
-        handle_message, title=agent.name, description=agent.description, retry_btn=None, undo_btn=None, clear_btn=None
-    )
+    return gr.ChatInterface(handle_message, type="messages", title=agent.name, description=agent.description)
