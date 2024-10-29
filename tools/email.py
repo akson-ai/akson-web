14a1432b765566bb13c7736962ac9f5ea17a80c6
Cenk Altı <cenkalti@gmail.com>
2024-10-29 16:14:27 -0400

    add content writer tool

--- tools/email.py
+++ tools/email.py
@@ -8,12 +8,12 @@ from langchain_core.tools import tool
 
 @tool
 def send_email(to: str, subject: str, body: str):
-    """Sends an email."""
+    """Sends an email. Input is treated as HTML."""
     msg = MIMEMultipart()
     msg["From"] = os.environ["ZOHO_SMTP_EMAIL"]
     msg["To"] = to
     msg["Subject"] = subject
-    msg.attach(MIMEText(body, "plain"))
+    msg.attach(MIMEText(body, "html"))
 
     with smtplib.SMTP_SSL("smtp.zoho.com", 465) as server:
         server.login(os.environ["ZOHO_SMTP_EMAIL"], os.environ["ZOHO_SMTP_PASSWORD"])
