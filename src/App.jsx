08d582b3618105c29844100feb20f23fb8f851dd
Cenk Altı <cenkalti@gmail.com>
2025-04-11 13:53:48 -0400

    remove todos

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,6 @@ import ChatContent from "./components/ChatContent";
 import KeyboardShortcutsModal from "./components/KeyboardShortcutsModal";
 import { API_BASE_URL } from "./constants";
 
-// TODO allow editing of messages
-// TODO allow forking of chats
-// TODO when new chat is persisted, reload chat history
-// TODO fix jumping to button when streaming
-// TODO add "trim history" button
-// TODO add "summarize history" button
-// TODO format and colorize structured output chat bubbles
-
 // Redirect from root path to /chat with a new UUID
 // or generate new UUID if at /chat without ID
 if (
