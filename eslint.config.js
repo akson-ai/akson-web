5fadfdf45e8598556904f5eb4003c97afaee6d07
Cenk Altı <cenkalti@gmail.com>
2025-04-11 01:57:19 -0400

    install react-query

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default [
+  ...pluginQuery.configs['flat/recommended'],
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
